633b1ce0448cc663fac926ee1535941b552435a5
Jota Vares <joe@eternallynocturnal.com.com>
2016-05-04 09:16:16 -0400

    master commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>As We Divide</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                background: url('./images/aswed.jpg') no-repeat center center fixed;
+                -webkit-background-size: cover; /* For WebKit*/
+                -moz-background-size: cover;    /* Mozilla*/
+                -o-background-size: cover;      /* Opera*/
+                background-size: cover;         /* Generic*/
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Open Sans Condensed', sans-serif;
+            }
+
+            .container {
+                text-align: right;
+                display: table-cell;
+            }
+
+            .content {
+                text-align: center;
+                background-color: #fff;
+                padding:15px;
+                border-radius: 10px;
+                opacity: .8;
+                display: inline-block;
+                margin-right: 4%;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Bandcamp? Why pay them?</div>
+            </div>
+        </div>
+    </body>
+</html>
